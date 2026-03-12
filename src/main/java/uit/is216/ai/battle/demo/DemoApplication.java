8ef8288bbf2dd8d935b74b64ee1b0fab0fdba7eb
Minh Phan Dinh <phandinhminh48@gmail.com>
2026-03-12 22:18:54 +0700

    first commit

---
+++ src/main/java/uit/is216/ai/battle/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package uit.is216.ai.battle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uit/is216/ai/battle/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uit.is216.ai.battle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
